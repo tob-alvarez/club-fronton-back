12f5d0576f4f9cd2dbf27985ec60bf4af7d3c356
tob-alvarez <tosal1099@gmail.com>
2025-06-27 20:34:06 -0300

    cambio de dominio del front

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(cors({
-    origin: "http://localhost:3000", // dominio del frontend
+    origin: "https://v0-fronton-accounting-jonl1o9qo-tobias-alvarezs-projects.vercel.app/", // dominio del frontend
     credentials: true,
 }));
 
